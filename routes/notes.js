5c1fb90e0530fcbf3614cf48ce37570bfc1af820
ibrahim <ibrahimmuhieddine02@gmail.com>
2023-08-21 19:57:10 -0700

    I had to make a new REPO the old one will be linked to the readme

---
+++ routes/notes.js
@@ -0,0 +1,47 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend, writeToFile} = require('../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
+
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new note
+notes.post('/', (req, res) => {
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+// DELETE Route to erase any notes
+notes.delete('/:id', (req, res) => {
+  const noteId = req.params.id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.id !== noteId);
+      writeToFile('./db/db.json', result);
+    })
+    .catch((error)=>{
+    console.log(error);
+    console.log ('There was a problem getting to the db')
+    });
+ 
+
+});
+
+
+module.exports = notes;
